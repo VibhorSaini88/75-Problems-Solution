0e300d8ea2e6908f7e18b5b15f59be459a36cf39
vibhor-config <vibhorsaini14@gmail.com>
2025-09-29 00:38:42 +0530

    Union of two sorted arrays

---
+++ Union_of_two_sorted_arrays.java
@@ -0,0 +1,50 @@
+package EveryDay75;
+
+
+import java.util.*;
+
+public class Union_of_two_sorted_arrays {
+
+    public static void main(String[] args) {
+        int[] farr = {99, 100, 25, -3, 42, 0, 17, -99, 56, 25, -7, 99};
+        int[] sarr = {99, 100, 255, -4, 42, 0, 17, -999, 56, 25, -77, 99};
+
+        int count = 0;
+        int[] uniarr  = new int[farr.length+sarr.length];
+        for(int index=0; index<farr.length; index++){
+            uniarr[index] = farr[index];
+            count += 1;
+        }
+        int next = count;
+        for(int index=0; index<sarr.length; index++){
+            uniarr[next] = sarr[index];
+            next++;
+            count += 1;
+        }
+        uniarr = Arrays.stream(uniarr).distinct().toArray();
+        System.out.println(Arrays.toString(uniarr)+"\n"+count);
+
+        Set<Integer> set = new LinkedHashSet<>();
+        for(int value: farr){
+            set.add(value);
+        }
+        for(int value: sarr){
+            set.add(value);
+        }
+        int[] unionArr = set.stream().mapToInt(Integer::intValue).toArray();
+        System.out.println("set of first easy way: "+Arrays.toString(unionArr));
+
+        List<Integer> flist = Arrays.stream(farr).boxed().toList();
+        List<Integer> slist = Arrays.stream(sarr).boxed().toList();
+        Set<Integer> finalset = new HashSet<>(flist);
+        finalset.addAll(slist);
+        System.out.println(finalset);
+
+
+        farr = Arrays.stream(farr).distinct().sorted().toArray();
+        sarr = Arrays.stream(sarr).distinct().sorted().toArray();
+        System.out.println(Arrays.toString(farr)+"\n"+Arrays.toString(sarr));
+
+
+    }
+}
